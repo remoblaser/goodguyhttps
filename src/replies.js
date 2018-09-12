a619bcc54a687a004c8e61326f9a41265c5f8294
Remo Blaser <remo.blaser@gmail.com>
2018-09-12 09:42:16 +0200

    Added reply

--- src/replies.js
+++ src/replies.js
@@ -2,5 +2,6 @@ module.exports = [
   "Hey there! 👋 Just here to inform you that you should switch to HTTPS. Here's why: https://doesmysiteneedhttps.com",
   "Your website isn't using HTTPS yet, you should switch as soon as possible 🤙. Here's why: https://doesmysiteneedhttps.com",
   "Nice link 👍! But make sure you include HTTPS soon. Here's why: https://doesmysiteneedhttps.com",
-  "Ooooph 😥 seems like that website is not using HTTPS, make sure to get a certificate. Here's why: https://doesmysiteneedhttps.com"
+  "Ooooph 😥 seems like that website is not using HTTPS, make sure to get a certificate. Here's why: https://doesmysiteneedhttps.com",
+  "Cool! But make sure you'll get a SSL certificate soon. Here's why: https://doesmysiteneedhttps.com"
 ]
